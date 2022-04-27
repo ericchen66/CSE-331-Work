5480caff4924b5a841e6d28b554602d29963f9bd
eric66 <eric66@cs.washington.edu>
2022-04-27 15:11:24 -0700

    Added unit tests for union, intersection, and difference.

--- hw-sets/src/test/java/sets/FiniteSetTest.java
+++ hw-sets/src/test/java/sets/FiniteSetTest.java
@@ -52,29 +52,57 @@ public class FiniteSetTest {
     assertEquals(S1.size(), 1);
     assertEquals(S12.size(), 2);
   }
-  
+
+  private static FiniteSet T1 = FiniteSet.of(new float[] {2, 3, 4});
+  private static FiniteSet T2 = FiniteSet.of(new float[] {3, 4, 5});
+  private static FiniteSet T3 = FiniteSet.of(new float[] {2, 3, 4, 5});
+  private static FiniteSet T4 = FiniteSet.of(new float[] {3, 4});
+  private static FiniteSet T5 = FiniteSet.of(new float[] {5, 6});
+  private static FiniteSet T6 = FiniteSet.of(new float[] {3, 4, 5, 6});
+  private static FiniteSet T7 = FiniteSet.of(new float[] {2});
   // TODO: Feel free to initialize (private static) FiniteSet objects here
   //       if you plan to use them for the tests below.
 
   /** Tests forming the union of two finite sets. */
   @Test
   public void testUnion() {
-    // TODO: implement this
-    
+    assertTrue(S0.union(S0).equals(S0));
+    assertTrue(S1.union(S1).equals(S1));
+    assertTrue(S0.union(S1).equals(S1));
+    assertTrue(S1.union(S12).equals(S12));
+    assertTrue(S1.union(T7).equals(S12));
+    assertFalse(S0.union(S1).equals(S12));
+
+    assertTrue(T1.union(T2).equals(T3));
+    assertTrue(T4.union(T5).equals(T6));
+    assertFalse(T1.union(T2).equals(T4));
   }
 
   /** Tests forming the intersection of two finite sets. */
   @Test
   public void testIntersection() {
-    // TODO: implement this
-    
+    assertTrue(S0.intersection(S0).equals(S0));
+    assertTrue(S1.intersection(S1).equals(S1));
+    assertFalse(S0.intersection(S1).equals(S12));
+
+    assertTrue(T1.intersection(T2).equals(T4));
+    assertTrue(T4.intersection(T5).equals(S0));
+    assertTrue(T3.intersection(T2).equals(T2));
+    assertFalse(T1.intersection(T2).equals(T3));
+    assertFalse(T2.intersection(T3).equals(T4));
   }
 
   /** Tests forming the difference of two finite sets. */
   @Test
   public void testDifference() {
-    // TODO: implement this
-    
+    assertTrue(S1.difference(S1).equals(S0));
+    assertTrue(S0.difference(S0).equals(S0));
+
+    assertTrue(T6.difference(T5).equals(T4));
+    assertTrue(T1.difference(T2).equals(T7));
+    assertTrue(T4.difference(T5).equals(T4));
+    assertFalse(T5.difference(T4).equals(T4));
+    assertFalse(T4.difference(T5).equals(T6));
   }
 
 }
